439f336338c3f9e8cdfdbb66c411dc487a59c56a
iMac-05 <imac@iMac-05s-iMac.local>
2017-08-21 14:23:39 +0700

    change bootstrap

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -7,17 +7,15 @@
 Route::get('/', 'FrontendController@index')->name('frontend.index');
 
 
-<<<<<<< 7eb2e3ed803c0c26a9e5753312d2a4240b069e16
 Route::get('List/I3', 'FrontendController@Student_List_I3')->name('frontend.Student_List_I3');
 Route::get('List/I4', 'FrontendController@Student_List_I4')->name('frontend.Student_List_I4');
-=======
+
 
 Route::get('mission-main', 'FrontendController@mission_main')->name('frontend.mission.main');
 Route::get('mission-detail', 'FrontendController@mission_detail')->name('frontend.mission.detail');
 
 
 Route::get('homepage', 'FrontendController@homepage')->name('frontend.homepage');
->>>>>>> update Mission and bootstrap
 
 
 
